a3a21ef2988096102586dd1db7b461c7da31d39e
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-01-23 21:00:02 +0000

    mobile menu js done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const menuIconEl = document.querySelector('#menu-icon');
 const menuEl = document.querySelector('.menu-div-mobile');
 const crossIconEl = document.querySelector('#cross-icon');
-// console.log(menuIconEl, menuEl, crossIconEl);
+const featuresMobClick = document.querySelector('.features-mobile-click');
+const featuresMenuDivMob = document.querySelector('.features-menu');
+const companyMobClick = document.querySelector('.company-mobile-click');
+const companyMenuDivMob = document.querySelector('.company-menu');
+// console.log('companyMobClick', companyMobClick);
+// console.log(companyMenuDivMob);
 
 menuIconEl.addEventListener('click', e => {
   menuEl.classList.remove('off').add('on');
@@ -9,3 +14,23 @@ menuIconEl.addEventListener('click', e => {
 crossIconEl.addEventListener('click', e => {
   menuEl.classList.add('off');
 });
+
+featuresMobClick.addEventListener('click',
+  e => {
+    featuresMenuDivMob.classList.contains('d-none')
+      ? featuresMenuDivMob.classList.remove('d-none').add('d-block')
+      : featuresMenuDivMob.classList.add('d-none');
+
+  });
+companyMobClick.addEventListener('click',
+  e => {
+    companyMenuDivMob.classList.contains('d-none')
+      ? companyMenuDivMob.classList.remove('d-none').add('d-block')
+      : companyMenuDivMob.classList.add('d-none');
+
+  });
+// function openClose(e) {
+//   e.target.classList.contains('d-none')
+//     ? e.target.classList.remove('d-none').add('d-block')
+//     : e.target.classList.add('d-none');
+// }
