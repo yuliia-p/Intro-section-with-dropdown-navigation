f32e952f5fa242f16a9a67a35f45b3c1a1fdc963
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-01-23 22:48:04 +0000

    desktop js done

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const featuresMobClick = document.querySelector('.features-mobile-click');
 const featuresMenuDivMob = document.querySelector('.features-menu');
 const companyMobClick = document.querySelector('.company-mobile-click');
 const companyMenuDivMob = document.querySelector('.company-menu');
-// console.log('companyMobClick', companyMobClick);
-// console.log(companyMenuDivMob);
+const featuresDesktopClick = document.querySelector('.features-click');
+const featuresMenuDivDesktop = document.querySelector('.features-menu-destop');
+const companyDesktopClick = document.querySelector('.company-click');
+const companyMenuDivDesktop = document.querySelector('.company-menu-destop');
 
 menuIconEl.addEventListener('click', e => {
   menuEl.classList.remove('off').add('on');
@@ -20,17 +22,22 @@ featuresMobClick.addEventListener('click',
     featuresMenuDivMob.classList.contains('d-none')
       ? featuresMenuDivMob.classList.remove('d-none').add('d-block')
       : featuresMenuDivMob.classList.add('d-none');
-
   });
 companyMobClick.addEventListener('click',
   e => {
     companyMenuDivMob.classList.contains('d-none')
       ? companyMenuDivMob.classList.remove('d-none').add('d-block')
       : companyMenuDivMob.classList.add('d-none');
-
   });
-// function openClose(e) {
-//   e.target.classList.contains('d-none')
-//     ? e.target.classList.remove('d-none').add('d-block')
-//     : e.target.classList.add('d-none');
-// }
+featuresDesktopClick.addEventListener('click', e => {
+  featuresMenuDivDesktop.classList.contains('d-none')
+    ? featuresMenuDivDesktop.classList.remove('d-none').add('d-block')
+    : featuresMenuDivDesktop.classList.add('d-none');
+
+});
+companyDesktopClick.addEventListener('click',
+  e => {
+    companyMenuDivDesktop.classList.contains('d-none')
+      ? companyMenuDivDesktop.classList.remove('d-none').add('d-block')
+      : companyMenuDivDesktop.classList.add('d-none');
+  });
