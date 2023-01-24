3b8bd15f3fe6dfa7449d00d8dcda2cd011486ba8
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-01-24 17:58:09 +0000

    main.js improved

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 const menuIconEl = document.querySelector('#menu-icon');
 const menuEl = document.querySelector('.menu-div-mobile');
 const crossIconEl = document.querySelector('#cross-icon');
-const featuresMobClick = document.querySelector('.features-mobile-click');
-const featuresMenuDivMob = document.querySelector('.features-menu');
-const companyMobClick = document.querySelector('.company-mobile-click');
-const companyMenuDivMob = document.querySelector('.company-menu');
-const featuresDesktopClick = document.querySelector('.features-click');
-const featuresMenuDivDesktop = document.querySelector('.features-menu-destop');
-const companyDesktopClick = document.querySelector('.company-click');
-const companyMenuDivDesktop = document.querySelector('.company-menu-destop');
+const elToClick = document.querySelectorAll('.click');
 
 menuIconEl.addEventListener('click', e => {
   menuEl.classList.remove('off').add('on');
@@ -17,27 +10,10 @@ crossIconEl.addEventListener('click', e => {
   menuEl.classList.add('off');
 });
 
-featuresMobClick.addEventListener('click',
-  e => {
-    featuresMenuDivMob.classList.contains('d-none')
-      ? featuresMenuDivMob.classList.remove('d-none').add('d-block')
-      : featuresMenuDivMob.classList.add('d-none');
+elToClick.forEach(function (item) {
+  item.addEventListener('click', function () {
+    item.nextElementSibling.classList.contains('d-none')
+      ? item.nextElementSibling.classList.remove('d-none').add('d-block')
+      : item.nextElementSibling.classList.add('d-none');
   });
-companyMobClick.addEventListener('click',
-  e => {
-    companyMenuDivMob.classList.contains('d-none')
-      ? companyMenuDivMob.classList.remove('d-none').add('d-block')
-      : companyMenuDivMob.classList.add('d-none');
-  });
-featuresDesktopClick.addEventListener('click', e => {
-  featuresMenuDivDesktop.classList.contains('d-none')
-    ? featuresMenuDivDesktop.classList.remove('d-none').add('d-block')
-    : featuresMenuDivDesktop.classList.add('d-none');
-
 });
-companyDesktopClick.addEventListener('click',
-  e => {
-    companyMenuDivDesktop.classList.contains('d-none')
-      ? companyMenuDivDesktop.classList.remove('d-none').add('d-block')
-      : companyMenuDivDesktop.classList.add('d-none');
-  });
